85b66f0f78b1f7e4a73477cf412545eab086b173
alowhiter <alowhiter0321@outlook.com>
2024-09-09 13:56:03 +0800

    let IndeterminateDelay.java like newer version

--- src/main/java/ch/njol/skript/effects/IndeterminateDelay.java
+++ src/main/java/ch/njol/skript/effects/IndeterminateDelay.java
@@ -46,15 +46,20 @@ public class IndeterminateDelay extends Delay {
 			final Timespan d = duration.getSingle(e);
 			if (d == null)
 				return null;
+
+			// Back up local variables
 			Object localVars = Variables.removeLocals(e);
+
 			Bukkit.getScheduler().scheduleSyncDelayedTask(Skript.getInstance(), new Runnable() {
 				@Override
 				public void run() {
 					if (Skript.debug())
 						Skript.info(getIndentation() + "... continuing after " + (System.nanoTime() - start) / 1000000000. + "s");
+
 					// Re-set local variables
 					if (localVars != null)
 						Variables.setLocalVariables(e, localVars);
+
 					TriggerItem.walk(next, e);
 				}
 			}, d.getTicks_i());
