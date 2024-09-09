87e27cf450993b174ff43570b7023da6ab15cb79
alowhiter <alowhiter0321@outlook.com>
2024-09-09 13:58:49 +0800

    let Trigger.java like newer version

--- src/main/java/ch/njol/skript/lang/Trigger.java
+++ src/main/java/ch/njol/skript/lang/Trigger.java
@@ -54,7 +54,21 @@ public class Trigger extends TriggerSection {
 	 */
 	public boolean execute(final Event e) {
 		boolean success = TriggerItem.walk(this, e);
+		// Clear local variables
 		Variables.removeLocals(e);
+		/*
+		 * Local variables can be used in delayed effects by backing reference
+		 * of VariablesMap up. Basically:
+		 *
+		 * Object localVars = Variables.removeLocals(e);
+		 *
+		 * ... and when you want to continue execution:
+		 *
+		 * Variables.setLocalVariables(e, localVars);
+		 *
+		 * See Delay effect for reference.
+		 */
+
 		return success;
 	}
 	
